bf9086de1d8b97c9d074057f8f371c57b29e7e7e
ayoung-jeon <wjsdkdud71@naver.com>
2023-11-10 21:42:44 +0900

    최종 정리 및 조건 추가

--- src/main/java/com/sparta/auth/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/auth/dto/SignupRequestDto.java
@@ -3,6 +3,8 @@ package com.sparta.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -16,6 +18,9 @@ public class SignupRequestDto {
     private String username;
 
     @NotBlank
+    @Size(min = 8, max = 15, message = "비밀번호는 최소 8자 이상, 15자 이하이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,15}$",
+            message = "비밀번호는 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 구성되어야 합니다.")
     private String password;
 
     @Email(message = "이메일 형식에 맞게 입력하세요")
